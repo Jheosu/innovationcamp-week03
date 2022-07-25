65f7a762f3c698fb8cc38ae408158059ab635e63
Jheosu <little8770@naver.com>
2022-07-25 14:28:21 +0900

    github

--- myblog/src/main/java/com/innovation/myblog/controller/MyblogController.java
+++ myblog/src/main/java/com/innovation/myblog/controller/MyblogController.java
@@ -40,9 +40,7 @@ public class MyblogController {
 
     @GetMapping("/api/posts")
     public List<Myblog> getposts() {
-
         return myblogRepository.findAll();
-
     }
 
     @PutMapping("api/posts/{id}")
