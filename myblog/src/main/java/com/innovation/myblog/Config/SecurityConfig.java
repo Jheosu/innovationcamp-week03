5b3c19da42360af20dc9408a2d378d8af69b74ea
kdy-git <zinee2001@naver.com>
2022-08-05 21:23:47 +0900

    s3 - project 연동. file upload test 완료

--- myblog/src/main/java/com/innovation/myblog/Config/SecurityConfig.java
+++ myblog/src/main/java/com/innovation/myblog/Config/SecurityConfig.java
@@ -59,6 +59,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .authorizeRequests()
                 .antMatchers("/auth/**").permitAll()
                 .antMatchers("/api/posts/**").permitAll()
+                .antMatchers("/upload").permitAll()
                 .anyRequest().authenticated()   // 나머지 API 는 전부 인증 필요
 
                 // JwtFilter 를 addFilterBefore 로 등록했던 JwtSecurityConfig 클래스를 적용
--- myblog/src/main/java/com/innovation/myblog/controller/AwsController.java
+++ myblog/src/main/java/com/innovation/myblog/controller/AwsController.java
@@ -1,2 +1,21 @@
-package com.innovation.myblog.controller;public class AwsController {
+package com.innovation.myblog.controller;
+
+import com.innovation.myblog.service.AwsService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+@RequiredArgsConstructor
+@RestController
+public class AwsController {
+
+    private final AwsService awsService;
+
+    @PostMapping("/upload")
+    public String upload(@RequestParam("file") MultipartFile multipartFile) throws Exception {
+        awsService.uploadFile(multipartFile, "static");
+        return "test";
+    }
 }
