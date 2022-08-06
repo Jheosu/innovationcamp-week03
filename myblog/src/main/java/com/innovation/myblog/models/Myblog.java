d4e6652e938665c48abb005c84f1bbd1ac788cd4
Jheosu <little8770@naver.com>
2022-08-06 14:05:46 +0900

    commit

--- myblog/src/main/java/com/innovation/myblog/models/Myblog.java
+++ myblog/src/main/java/com/innovation/myblog/models/Myblog.java
@@ -7,6 +7,10 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+import static javax.persistence.CascadeType.ALL;
 
 
 @NoArgsConstructor
